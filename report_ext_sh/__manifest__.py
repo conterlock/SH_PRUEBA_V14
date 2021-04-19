29b1e975d063f458259bb91d81fa120ed7fadeb3
Luis Millan <lmillan131@gmail.com>
2021-04-19 17:18:21 -0400

    inicio de reporte

---
+++ report_ext_sh/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Extención de Reportes",
+    "version": "1.0",
+    "author": "Soluciones SoftHard",
+    "license": "AGPL-3",
+    "category": 'Localization',
+    "depends": [
+        "base",
+    ],
+    'data': [
+        'report/report_sale_order.xml'
+    ],
+    'installable': True,
+}
